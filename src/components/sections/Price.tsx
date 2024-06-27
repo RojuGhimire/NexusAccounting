29aef42e8e71fc7066026bd2289d3f04636206d0
Roju Ghimire <ghimireroju@gmail.com>
2024-06-27 21:21:33 +0545

    changes

--- src/components/sections/Price.tsx
+++ src/components/sections/Price.tsx
@@ -1,5 +1,56 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React from 'react';
+import { motion } from 'framer-motion';
+
+interface PlanProps {
+  imgSrc: string;
+  buttonText: string;
+  price: string;
+  features: string[];
+  additionalInfo?: string;
+}
+
+const Plan: React.FC<PlanProps> = ({ imgSrc, buttonText, price, features, additionalInfo }) => {
+  const hoverVariants = {
+    hover: {
+      scale: 1.05,
+      transition: { duration: 0.3 },
+    },
+  };
+
+  return (
+    <motion.div
+      className="bg-card text-card-foreground p-6 rounded-lg shadow-lg w-full md:w-[300px] lg:w-[300px] h-auto gap-8 bg-[#F0F9F8] mx-2 my-4"
+      whileHover="hover"
+      variants={hoverVariants}
+    >
+      <motion.img
+        src={imgSrc}
+        alt="business-rhino"
+        className="mx-auto h-[119px] w-[119px]"
+        variants={hoverVariants}
+      />
+      <button className="bg-[#16AF9D] text-white h-[39px] py-2 px-4 rounded mb-2 mx-auto block">
+        {buttonText}
+      </button>
+      {price && <p className="text-[#E20000] text-center mt-4 font-extrabold">{price}</p>}
+      <div className="bg-white h-[257px] w-full mt-5 p-3 rounded-lg shadow-inner items-center justify-center">
+        <p className="text-[#16AF9D] mb-2">{additionalInfo || 'Including'}</p>
+        <ul className="mb-4">
+          {features.map((feature, index) => (
+            <li key={index}className="flex items-center mb-2">
+            <img src="/Check.png" alt="Check" className="h-4 w-4 mr-2" />
+            {feature}</li>
+          ))}
+        </ul>
+        {buttonText === 'THE BIG NEXUS ADVISOR' ? (
+          <p className="text-[#E20000] mt-16 text-xs font-bold leading-4 text-left">Contact us for a personalized quote.</p>
+        ) : (
+          <button className="bg-black text-white py-2 px-4 rounded items-center justify-center w-full">Start</button>
+        )}
+      </div>
+    </motion.div>
+  );
+};
 
 const Price: React.FC = () => {
   const containerVariants = {
@@ -8,7 +59,7 @@ const Price: React.FC = () => {
       opacity: 1,
       y: 0,
       transition: {
-        type: "spring",
+        type: 'spring',
         stiffness: 100,
         damping: 10,
         staggerChildren: 0.3,
@@ -22,154 +73,93 @@ const Price: React.FC = () => {
       opacity: 1,
       y: 0,
       transition: {
-        type: "spring",
+        type: 'spring',
         stiffness: 100,
         damping: 10,
       },
     },
   };
 
-  const hoverVariants = {
-    hover: {
-      scale: 1.05,
-      transition: { duration: 0.3 },
-    },
-  };
-
   return (
     <motion.div
       id="pricing"
-      className="max-w-[1440px] h-auto overflow-hidden  gap-8 w-full mx-auto px-4 py-16"
+      className="max-w-[1440px] h-auto overflow-hidden gap-8 w-full mx-auto px-4 py-16"
       initial="hidden"
       animate="visible"
       variants={containerVariants}
     >
-      <div className="text-center">
-        <motion.h2
-          className="text-3xl font-bold text-zinc-900"
-          variants={childVariants}
-        >
+      <div className="text-center mt-12">
+        <motion.h2 className="text-3xl font-bold text-zinc-900" variants={childVariants}>
           Pricing
         </motion.h2>
-        <motion.p className="text-zinc-600  mt-2" variants={childVariants}>
+        <motion.p className="text-zinc-600 mt-2" variants={childVariants}>
           Our pricing is tailored to your business size and specific needs:
         </motion.p>
-        <motion.div
-          className="border-b-2 border-zinc-600 w-16 mx-auto my-4"
-          variants={childVariants}
-        ></motion.div>
-        <motion.div
-          className="border-b-2 border-zinc-600 w-16 mx-auto my-4"
-          variants={childVariants}
-        ></motion.div>
-        <motion.p
-          className="text-zinc-600 font-bold italic mt-2"
-          variants={childVariants}
-        >
-          Choose from our flexible packages designed to accommodate businesses
-          of all scales.
-        </motion.p>
+        <motion.div className="border-b-2 border-zinc-600 w-16 mx-auto my-4" variants={childVariants}>
+        </motion.div>
+        <motion.div className="border-b-2 border-zinc-600 w-16 mx-auto my-4" variants={childVariants}>
+        </motion.div>
       </div>
 
-      <div className="flex flex-wrap justify-center gap-[70px] p-4">
+      <div className="flex flex-wrap justify-center gap-4 lg:gap-8 p-4">
         {[
           {
-            title: "SAVE A NEXUS ASSISTANT",
-            price: "$300/Month",
+            imgSrc: '/rhinoo.png',
+            buttonText: 'SAVE A NEXUS ASSISTANT',
+            price: '$300/ Month',
             features: [
-              "Once a week processing",
-              "Quickbooks Setup",
-              "Monthly Transaction Review",
-              "Direct Text Line",
-              "Bookkeeping Concierge “Rhino”",
+              'Once a week processing',
+              'Quickbooks Setup',
+              'Monthly Transaction Review',
+              'Direct Text Line',
+              'Bookkeeping Concierge “Rhino”',
             ],
-            imageSrc: "/rhinoo.png",
-            altText: "Nexus Assistant",
           },
           {
-            title: "MAKE A NEXUS ASSISTANT",
-            price: "$600/Month",
+            imgSrc: '/rhinoo.png',
+            buttonText: 'MAKE A NEXUS ASSISTANT',
+            price: '$600/ Month',
             features: [
-              "Twice a week processing",
-              "W2 management",
-              "Light A/R & A/P",
-              "+ Year-End Processing",
+              'Twice a week processing',
+              'W2 management',
+              'Light A/R & A/P',
+              '+ Year-End Processing',
             ],
-            imageSrc: "/rhinoo.png",
-            altText: "Nexus Assistant",
+            additionalInfo: 'Save Membership',
           },
           {
-            title: "GIVE A NEXUS EXECUTIVE",
-            price: "$1,200/Month",
+            imgSrc: '/rhinoo.png',
+            buttonText: 'GIVE A NEXUS EXECUTIVES',
+            price: '$1,200/ Month',
             features: [
-              "Large-Scale Monthly Bookkeeping",
-              "4 times a week processing",
-              "A/R & A/P",
-              "+ Year-End Processing",
+              'Large-Scale Monthly Bookkeeping',
+              '4 times a week processing',
+              'A/R & A/P',
+              '+ Year-End Processing',
             ],
-            imageSrc: "/rhinoo.png",
-            altText: "Nexus Executive",
+            additionalInfo: 'Save Membership',
           },
           {
-            title: "THE BIG NEXUS ADVISOR",
-            price: "Contact Us",
+            imgSrc: '/rhinoo.png',
+            buttonText: 'THE BIG NEXUS ADVISOR',
+            price: 'Contact Us',
             features: [
-              "Co-Founders?",
-              "Financial Management?",
-              "Capital Fundraising?",
-              "Equity Investment?",
+              'Co-Founders?',
+              'Financial Management?',
+              'Capital Fundraising?',
+              'Equity Investment?',
             ],
-            imageSrc: "/rhinoo.png",
-            altText: "Nexus Advisor",
+            additionalInfo: 'Contact Us',
           },
         ].map((plan, index) => (
-          <motion.div
+          <Plan
             key={index}
-            className="bg-[#F0F9F8] rounded-lg mt-16 shadow-xl p-6 w-full sm:w-1/2 lg:w-[271px] "
-            variants={childVariants}
-            whileHover="hover"
-          >
-            <motion.img
-              src={plan.imageSrc}
-              alt={plan.altText}
-              className="w-full h-48 object-cover rounded-t-lg"
-              variants={hoverVariants}
-            />
-            <div className="text-center mt-4">
-              <h2 className="text-xl font-bold">
-                {plan.title.includes("NEXUS") ? (
-                  <>
-                    {plan.title.split("NEXUS")[0]}
-                    <span className="text-teal-500 w-[70px] h-[26px] border-[2px] ">
-                      <br />
-                      NEXUS <br />
-                    </span>
-                    {plan.title.split("NEXUS")[1]}
-                  </>
-                ) : (
-                  plan.title
-                )}
-              </h2>
-              {plan.price !== "Contact Us" && (
-                <p className="text-red-500">{plan.price}</p>
-              )}
-            </div>
-            <div className="mt-4 p-4 bg-white shadow-sm rounded-lg">
-              <p className="text-teal-500 font-bold">
-                {plan.price === "Contact Us" ? "Contact Us" : "Including"}
-              </p>
-              <ul className="list-disc list-inside text-sm">
-                {plan.features.map((feature, idx) => (
-                  <li key={idx}>{feature}</li>
-                ))}
-              </ul>
-              {plan.price === "Contact Us" && (
-                <p className="text-red-500 mt-2">
-                  Contact us for a personalized quote.
-                </p>
-              )}
-            </div>
-          </motion.div>
+            imgSrc={plan.imgSrc}
+            buttonText={plan.buttonText}
+            price={plan.price}
+            features={plan.features}
+            additionalInfo={plan.additionalInfo}
+          />
         ))}
       </div>
     </motion.div>
